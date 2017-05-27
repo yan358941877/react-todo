26c6d5bcc86c1a443c6f9d7c25cec4b99be39a58
yan358941877 <yan358941877@163.com>
2017-05-27 09:46:37 +0800

    todo页面布局

--- src/containers/TodoPage/index.jsx
+++ src/containers/TodoPage/index.jsx
@@ -1,6 +1,9 @@
 /* module */
 import React from 'react'
 
+/* containers */
+import CalendarPart from './subpage/CalendarPart'
+import TodoPart from './subpage/TodoPart'
 /* style */
 import './style.css'
 
@@ -8,10 +11,14 @@ class TodoPage extends React.Component{
     render(){
         return(
             <div id="todo-page">
-                <div id="todo-container">
-                    <h2>Todo page</h2>
+                <div id="todo-container" className="clear-fix">
+                    <div id="todo-left" className="float-left">
+                        {/*<CalendarPart />*/}
+                    </div>
+                    <div id="todo-right" className="float-left">
+                        {/*<TodoPart />*/}
+                    </div>
                 </div>
-                
             </div>
             
         )
